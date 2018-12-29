06645bd01e5a55f4a8167fc488cacab72725ff34
SergeyMikhalev <sergey.v.mikhalev@gmail.com>
2018-12-29 20:07:25 +0300

    Домашняя работа к первому уроку

---
+++ Lesson1/Marathon/Obstacle/Water.java
@@ -0,0 +1,18 @@
+package Marathon.Obstacle;
+
+import Marathon.Competitor;
+import Marathon.Obstacle.Obstacle;
+
+public class Water extends Obstacle {
+    int length;
+
+    public Water(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public void doIt(Competitor competitor) {
+        competitor.swim(length);
+    }
+
+}
